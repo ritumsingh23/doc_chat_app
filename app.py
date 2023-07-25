f45e344deb792735ef4c501c4d6547f383cc2f9a
ankitanandk <ankitanand@ankits-mbp.lan>
2023-07-25 13:00:30 -0400

    Adding the codes to Flask_app

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import os
 from tempfile import NamedTemporaryFile
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 from langchain.llms import OpenAI
 from langchain.agents import create_csv_agent
 from dotenv import load_dotenv
+import const
 
-load_dotenv()
+#load_dotenv()
 
 app=Flask(__name__)
+
+openaiservicename = 'ms-openai-cosmos'
+os.environ["OPENAI_API_TYPE"] = "azure"
+os.environ["OPENAI_API_VERSION"] = '2023-05-15'
+os.environ["OPENAI_API_BASE"] = f"https://{openaiservicename}.openai.azure.com"
+os.environ["OPENAI_API_KEY"] = const.OPENAI_API_KEY 
+
+def database_connection():
+     db = SQLAlchemy()
+     db_name = ''
+
 
 @app.route('/')
 def index():
@@ -26,7 +40,7 @@ def predict_datapoint():
                     with NamedTemporaryFile() as f: # Create temporary file
                         f.write(user_csv.getvalue()) # Save uploaded contents to file
                         f.flush()
-                        llm=OpenAI(temperature=0)
+                        llm=OpenAI(engine='text-davinci-002', temperature=0)
                         user_question = request.form['query']
                         agent=create_csv_agent(llm, f.name, verbose=True)
 
