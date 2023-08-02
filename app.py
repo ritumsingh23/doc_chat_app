079d0c9714e2c19b2f8c005f628f68a0eacf3d7e
ankitanandk <ankit.anandk@gmail.com>
2023-08-02 12:43:54 -0400

    Testing changes to azure

--- app.py
+++ app.py
@@ -89,3 +89,99 @@ def predict_datapoint():
     
 if __name__=="__main__":
     app.run(host="0.0.0.0", port="4999",debug=True) 
+
+'''import os
+import sys
+from tempfile import NamedTemporaryFile
+from flask import Flask, request, render_template
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from langchain.llms import OpenAI
+from langchain.agents import create_csv_agent
+from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
+from utils import json_csv, get_blob_service_client, upload_to_blob_storage
+from exception import CustomException, querys
+#import const
+
+load_dotenv()
+
+app=Flask(__name__)
+
+openaiservicename = 'ms-openai-cosmos'
+os.environ["OPENAI_API_TYPE"] = "azure"
+os.environ["OPENAI_API_VERSION"] = '2023-05-15'
+os.environ["OPENAI_API_BASE"] = f"https://{openaiservicename}.openai.azure.com"
+os.environ["OPENAI_API_KEY"] = os.environ.get('OPENAI_API_KEY')
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'file_path/')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/predictdata', methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        try:
+            return render_template('home.html')
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+    else:
+
+        try:
+            if 'file' not in request.files:
+                return "No File Part"
+        
+            user_file = request.files['file']
+            
+            if user_file:
+                    
+                filename = secure_filename(user_file.filename)
+
+                temp_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                user_file.save(temp_file_path)
+
+                # Upload the file to Azure Blob storage
+                upload_to_blob_storage(temp_file_path, filename)
+
+                if filename.endswith(".csv"):
+                    #user_file.save(os.path.join(app.config['blob_path'], filename))      
+        
+                    with NamedTemporaryFile() as f: # Create temporary file
+                        f.write(user_file.getvalue()) # Save uploaded contents to file
+                        f.flush()
+                        llm=OpenAI(engine='text-davinci-002', temperature=0)
+                        user_question = request.form['query']
+                        agent=create_csv_agent(llm, f.name, verbose=True,  early_stopping_method="generate")
+
+                    if user_question is not None and user_question != "":
+                        response = querys(agent, user_question)
+
+                elif filename.endswith(".json"):
+                    #user_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  
+                    json_content = user_file.read()
+                    file_path = UPLOAD_FOLDER+filename
+                    save_file = UPLOAD_FOLDER+'test.csv'
+                    print(file_path, save_file)
+                    json_csv(file_path, save_file)
+                    #json_csv(UPLOAD_FOLDER, user_file.getvalue().decode('utf-8')) #converting the json file to csv for the csv agent
+                    llm=OpenAI(engine='text-davinci-002', temperature=0)
+                    user_question = request.form['query']
+                    #file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                    agent=create_csv_agent(llm, save_file, verbose=True, early_stopping_method="generate")
+
+                    if user_question is not None and user_question != "":
+                        response = querys(agent, user_question)
+
+                # Remove the temporary file after uploading
+                os.remove(temp_file_path)
+
+                return response
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port="4999",debug=True) '''
--- utils.py
+++ utils.py
@@ -55,3 +55,117 @@ def json_csv(path_to_store, file):
 
     df1.to_csv(os.path.join(path_to_store, 'test.csv'))
     upload_to_blob_storage(os.path.join(path_to_store, 'test.csv'), 'test.csv')
+
+'''import os 
+import sys
+import json
+import csv
+import pandas as pd
+from pandas import json_normalize
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from exception import CustomException
+
+Connection_String_to_blob = os.environ.get('Connection_String_to_blob')
+
+def get_blob_service_client():
+    connection_string = Connection_String_to_blob
+    return BlobServiceClient.from_connection_string(connection_string)
+
+def upload_to_blob_storage(file_path, filename):
+    blob_service_client = get_blob_service_client()
+    container_name = 'peearzchatdocupload'  # Create a container in your Blob storage account
+
+    # Get a blob client
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=filename)
+
+    # Upload the file to Blob storage
+    with open(file_path, 'rb') as data:
+        blob_client.upload_blob(data, overwrite=True)
+
+def fetch_file_from_blob(container_name, blob_name, destination_path):
+    blob_service_client = get_blob_service_client()
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_client = container_client.get_blob_client(blob_name)
+
+    with open(destination_path, "wb") as file:
+        blob_data = blob_client.download_blob()
+        file.write(blob_data.readall())
+
+def json_csv(path_to_store, file):
+
+    print("File Content:")
+    print(file)
+    try:
+        dictionary = json.loads(file)
+
+    except json.JSONDecodeError as e:
+        print("JSON Parsing Error : ", e)
+        print("FILE CONTENT : ")
+        print(file)
+        raise CustomException("Error parsing JSON content.", sys)
+    
+    except Exception as e:
+        print("Unexpected Error:", e)
+        raise CustomException("Unexpected error occurred while processing JSON content.", sys)    
+
+    df = None
+    first = True
+
+    for document in dictionary:
+        if document == None:
+            continue
+        else:
+            dfLeft = json_normalize(document)
+
+        for title, value in document.items():
+            if type(value) == list:
+                dfLeft.drop(title, axis="columns", inplace=True)
+                dfRight = json_normalize(value)
+                dfRight = dfRight.add_prefix(f"{title}_")
+                dfLeft = pd.concat([dfLeft, dfRight], axis = 1)    
+
+        if first:
+            df = dfLeft
+            first = False
+        else:
+            df = pd.concat([df, dfLeft], axis = 0)
+
+    df.reset_index(inplace=True, drop=True)
+
+    df1 = df.T.drop_duplicates().T
+
+    #file_location = os.path.join(app.config['UPLOAD_FOLDER'])
+    #df1.to_csv('file_path/test.csv')
+
+    df1.to_csv(os.path.join(path_to_store, 'test.csv'))
+    upload_to_blob_storage(os.path.join(path_to_store, 'test.csv'), 'test.csv')
+
+def json_csv(json_file_path, csv_file_path):
+    """
+    Convert a JSON file to a CSV file.
+
+    Parameters:
+        json_file_path (str): Path to the input JSON file.
+        csv_file_path (str): Path to the output CSV file.
+    """
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    with open(csv_file_path, 'w', newline='') as csv_file:
+        # Extract the headers from the first item in the JSON data
+        headers = list(data[0].keys())
+
+        # Create a CSV writer and write the header row
+        writer = csv.DictWriter(csv_file, fieldnames=headers)
+        writer.writeheader()
+
+        # Write each row to the CSV file
+        writer.writerows(data)
+
+def download_blob_to_file(container_name='peearzchatdocupload'):
+    blob_service_client = get_blob_service_client()
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob="test.csv")
+    with open(file=os.path.join(r'/Users/ankitanand/Documents/Peearz/', 'test.csv'), mode="wb") as sample_blob:
+        download_stream = blob_client.download_blob()
+        sample_blob =  download_stream.readall()
+        return sample_blob'''
